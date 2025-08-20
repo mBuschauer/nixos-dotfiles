c9bcc334308e7fd22a1a6bc24a479b240511716d
mBuschauer <marco@buschauer.us>
2025-08-20 13:38:30 -0400

    increase download-buffer-size

--- modules/admin/boot.nix
+++ modules/admin/boot.nix
@@ -9,18 +9,25 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernel = {
-      # modify TCP buffer size
-      sysctl."net.core.rmem_max" = 33554432;
-      sysctl."net.core.wmem_max" = 33554432;
-      # default                     4096	131072	6291456
-      sysctl."net.ipv4.tcp_rmem" = "4096  87380   33554432";
-      # default                     4096	16384	  4194304
-      sysctl."net.ipv4.tcp_wmem" = "4096  65536   33554432";
-    };
+    # kernel = {
+    #   # modify TCP buffer size
+    #   sysctl."net.core.rmem_max" = 33554432;
+    #   sysctl."net.core.wmem_max" = 33554432;
+    #   # default                     4096	131072	6291456
+    #   sysctl."net.ipv4.tcp_rmem" = "4096  87380   33554432";
+    #   # default                     4096	16384	  4194304
+    #   sysctl."net.ipv4.tcp_wmem" = "4096  65536   33554432";
+    # };
 
     # plymouth.enable = true; 
   };
+
+  nix = {
+    extraOptions = ''
+      download-buffer-size = 134217728 
+    '';
+  };
+
 
   services.greetd = {
     enable = true;
