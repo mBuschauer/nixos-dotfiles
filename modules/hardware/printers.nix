9e7a7464c6e316ead14252e7752e7eb966357988
mBuschauer <marco@buschauer.us>
2025-08-23 15:47:54 -0400

    fixed printers

--- modules/hardware/printers.nix
+++ modules/hardware/printers.nix
@@ -1,13 +1,17 @@
-{ pkgs, settings, ... }:
-{
+{ pkgs, settings, secrets, ... }: {
   # enable printing
   services.printing = {
-    enable = settings.customization.cups_enabled; 
-    drivers = with pkgs; [gutenprint hplip];
+    # http://localhost:631/
+    enable = settings.customization.cups_enabled;
+    drivers = with pkgs; [ gutenprint hplip ];
     openFirewall = settings.customization.cups_enabled;
   };
 
-  services.avahi  = {
+  hardware.printers = {
+    ensurePrinters = secrets.printers;
+  };
+
+  services.avahi = {
     enable = settings.customization.cups_enabled;
     ipv4 = true;
     ipv6 = true;
